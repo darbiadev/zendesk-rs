801ba9a05f94feb53fbd668f73a391bd001ae39b
shenanigansd <shenanigansd@users.noreply.github.com>
2022-07-01 13:13:45 +0000

    deploy: 587fa5e20afeec5edcb38834c15b72e2ecffa3c1

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
-var N = null;var sourcesIndex = {};
+var sourcesIndex = {};
 sourcesIndex["zendesk"] = {"name":"","dirs":[{"name":"resources","dirs":[{"name":"oauth_tokens","files":["mod.rs","models.rs"]},{"name":"tickets","files":["handlers.rs","mod.rs","models.rs"]}],"files":["mod.rs"]}],"files":["lib.rs"]};
 createSourceSidebar();
